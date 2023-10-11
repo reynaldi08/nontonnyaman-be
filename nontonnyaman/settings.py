8fa79b163ff1f3143f69050182ce594b7254aeaf
reynaldi0810 <reynaldi.oktavianus8@gmail.com>
2023-10-11 18:08:33 +1000

    fix detail

--- nontonnyaman/settings.py
+++ nontonnyaman/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -90,11 +90,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:9000",
     "http://localhost:58701",
-    "http://10.0.2.2",
+    "http://10.0.2.2:8000",
     "nonton-nyaman-cbfc2703b99d.herokuapp.com"
 ]
 CORS_ALLOW_HEADERS = "*"
 CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 WSGI_APPLICATION = 'nontonnyaman.wsgi.application'
 
